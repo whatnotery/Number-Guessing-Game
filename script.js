238a967c22b0ed8c506bd5b93208de8817938752
John O'shea <johncyoshea@me.com>
2021-09-23 18:20:56 -0400

    added reset button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const input = document.querySelector('#number');
 const button = document.querySelector('#button');
+const reset = document.querySelector('#reset');
 const h1 = document.querySelector('h1');
-let maxNum = 0
+let maxNum = 0;
 let targNum = 0;
-let buttonPress = 0
-let gameOver = false
+let buttonPress = 0;
+let gameOver = false;
 
 
 button.addEventListener('click', function (e) {
@@ -12,36 +13,40 @@ button.addEventListener('click', function (e) {
     if (buttonPress === 0) {
         let maxNum = input.value;
         targNum = Math.floor(Math.random() * maxNum) + 1;
-        h1.innerHTML = `Guess a number between 1 and ${maxNum}`;
-        buttonPress++
+        h1.innerHTML = `Guess a number <br> between 1 and ${maxNum}`;
+        button.innerHTML = 'Guess';
+        buttonPress++;
+        input.value = '';
     } else if (buttonPress >= 1 && !gameOver) {
+        buttonPress++;
         if (parseInt(input.value) > targNum) {
             h1.innerHTML = 'Too High, Guess again';
+            input.value = '';
+
         } else if (parseInt(input.value) < targNum) {
             h1.innerHTML = 'Too Low, Guess again';
+            input.value = '';
         } else if (parseInt(input.value) === targNum) {
-            h1.innerHTML = 'You Got It!!!';
             gameOver = true;
+            button.disabled = true;
+            if (buttonPress === 2) {
+            h1.innerHTML = `You got it! <br> It took you ${buttonPress -1} guess.`;
+        } else {
+            h1.innerHTML = `You got it! <br> It took you ${buttonPress -1} guesses.`;
         }
-
-}})
-
-
-
-
-/*
-
-
-
-
-
-
-
-else if (maxNum !== 0) {
-
-        let attempts = 1;
-        while (input.value !== targNum) {
-            attempts++;
-
+            
+        }
     }
-} */
+})
+
+reset.addEventListener('click', function () {
+    maxNum = 0;
+    targNum = 0;
+    buttonPress = 0;
+    input.value = ''
+    button.disabled = false;
+    gameOver = false;
+    button.innerHTML = 'Submit';
+    h1.innerHTML = 'Enter a max number';
+});
+
